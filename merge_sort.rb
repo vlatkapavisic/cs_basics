b3ba2026ca38cd6f135c7457a02e5bc9cf40b31d
Vlatka Pavisic <vlatka.pavisic@gmail.com>
2017-01-08 20:11:36 +0100

    Merge sort

---
+++ merge_sort.rb
@@ -0,0 +1,17 @@
+def mergesort(a)
+  return a if a.length == 1
+  middle = a.length/2
+  return merge(mergesort(a[0...middle]), mergesort(a[middle..-1]))
+end
+
+def merge(left, right)
+  subarray = []
+  until left.length == 0 || right.length == 0
+    subarray << (left.first <= right.first ? left.shift : right.shift)
+  end
+  subarray + left + right
+end
+
+
+a = *ARGV.map(&:to_i)
+puts mergesort(a)
